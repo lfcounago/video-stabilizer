427ba43a665c82e38dc531471d6c6c6721590761
Luis Fernández Couñago <103210160+luisfernandezcounago@users.noreply.github.com>
2023-02-08 19:27:38 +0100

    Add files via upload

---
+++ Proyecto_FernandezCounagoLuis/build/Debug/Cygwin-Windows/Proyecto_FernandezCounagoLuis.o.d
@@ -0,0 +1,44 @@
+build/Debug/Cygwin-Windows/Proyecto_FernandezCounagoLuis.o: \
+ Proyecto_FernandezCounagoLuis.c \
+ /cygdrive/C/opencv3.0.0/include/opencv/cv.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/core/core_c.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/core/types_c.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/core/cvdef.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/hal/defs.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/imgproc/imgproc_c.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/imgproc/types_c.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/photo/photo_c.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/video/tracking_c.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/objdetect/objdetect_c.h \
+ /cygdrive/C/opencv3.0.0/include/opencv/highgui.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/highgui/highgui_c.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/imgcodecs/imgcodecs_c.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/videoio/videoio_c.h
+
+/cygdrive/C/opencv3.0.0/include/opencv/cv.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/core/core_c.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/core/types_c.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/core/cvdef.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/hal/defs.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/imgproc/imgproc_c.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/imgproc/types_c.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/photo/photo_c.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/video/tracking_c.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/objdetect/objdetect_c.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv/highgui.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/highgui/highgui_c.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/imgcodecs/imgcodecs_c.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/videoio/videoio_c.h:
---
+++ Proyecto_FernandezCounagoLuis/build/Debug/Cygwin-Windows/main.o.d
@@ -0,0 +1,43 @@
+build/Debug/Cygwin-Windows/main.o: main.c \
+ /cygdrive/C/opencv3.0.0/include/opencv/cv.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/core/core_c.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/core/types_c.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/core/cvdef.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/hal/defs.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/imgproc/imgproc_c.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/imgproc/types_c.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/photo/photo_c.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/video/tracking_c.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/objdetect/objdetect_c.h \
+ /cygdrive/C/opencv3.0.0/include/opencv/highgui.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/highgui/highgui_c.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/imgcodecs/imgcodecs_c.h \
+ /cygdrive/C/opencv3.0.0/include/opencv2/videoio/videoio_c.h
+
+/cygdrive/C/opencv3.0.0/include/opencv/cv.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/core/core_c.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/core/types_c.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/core/cvdef.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/hal/defs.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/imgproc/imgproc_c.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/imgproc/types_c.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/photo/photo_c.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/video/tracking_c.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/objdetect/objdetect_c.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv/highgui.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/highgui/highgui_c.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/imgcodecs/imgcodecs_c.h:
+
+/cygdrive/C/opencv3.0.0/include/opencv2/videoio/videoio_c.h:
